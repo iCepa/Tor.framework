a0ee8975a128b47b1d211763150e7038d93a7761
Benjamin Erhart <be@benjaminerhart.com>
2017-11-27 16:01:57 +0100

    Added possibility to catch Tor/libevent logs with own handler.

--- Tor/TORLogging.h
+++ Tor/TORLogging.h
@@ -6,11 +6,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <os/log.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef void(* tor_log_cb)(os_log_type_t severity, const char* msg);
+
 extern void TORInstallEventLogging(void);
 
+extern void TORInstallEventLoggingCallback(tor_log_cb cb);
+
 extern void TORInstallTorLogging(void);
+
+extern void TORInstallTorLoggingCallback(tor_log_cb cb);
 
 NS_ASSUME_NONNULL_END
