ed2ef85c6bdc5fa93f6fdc6fe13254b473861af7
Mike Tigas <mike.tigas@gmail.com>
2018-10-02 15:01:59 -0400

    work in progress

--- Tor/tor.sh
+++ Tor/tor.sh
@@ -28,13 +28,47 @@ fi
 
 # If rebuilding or cleaning then delete the built products
 if [[ ${ACTION:-build} = "clean" ]] || [[ $REBUILD = 1 ]]; then
+    make clean 2>/dev/null
     make distclean 2>/dev/null
-    rm -r "${BUILT_PRODUCTS_DIR}/tor"* 2>/dev/null
-    rm "${BUILT_PRODUCTS_DIR}/libtor.a" 2>/dev/null
-    rm "${BUILT_PRODUCTS_DIR}/libor"*.a 2>/dev/null
-    rm "${BUILT_PRODUCTS_DIR}/libcurve25519_donna.a" 2>/dev/null
-    rm "${BUILT_PRODUCTS_DIR}/libed25519"*.a 2>/dev/null
-    rm "${BUILT_PRODUCTS_DIR}/libkeccak-tiny.a" 2>/dev/null
+    for LIB in \
+        libed25519_ref10 \
+        libed25519_donna \
+        libkeccak-tiny \
+        libtor-err \
+        libtor-ctime \
+        libtor-compress \
+        libtor-container \
+        libtor-crypt-ops \
+        libtor-encoding \
+        libtor-evloop \
+        libtor-fdio \
+        libtor-fs \
+        libcurve25519_donna \
+        libtor-intmath \
+        libtor-lock \
+        libtor-log \
+        libtor-math \
+        libtor-memarea \
+        libtor-meminfo \
+        libtor-malloc \
+        libtor-net \
+        libtor-osinfo \
+        libtor-process \
+        libtor-sandbox \
+        libtor-string \
+        libtor-smartlist-core \
+        libtor-term \
+        libtor-thread \
+        libtor-time \
+        libtor-tls \
+        libtor-trace \
+        libtor-wallclock \
+        libor-trunnel \
+        libtorrunner \
+        libtor-app;
+    do
+        rm "${BUILT_PRODUCTS_DIR}/${LIB}.a" 2> /dev/null
+    done
 fi
 
 if [[ $REBUILD = 0 ]]; then
@@ -61,11 +95,13 @@ mkdir -p "${BUILT_PRODUCTS_DIR}"
 # Build each architecture one by one using clang
 for ARCH in "${ARCHS[@]}"
 do
+    make clean
     make distclean
 
-    ./configure --enable-restart-debugging --disable-tool-name-check --disable-unittests --enable-static-openssl --enable-static-libevent --disable-asciidoc --disable-system-torrc --disable-linker-hardening --disable-dependency-tracking --prefix="${CONFIGURATION_TEMP_DIR}/tor-${ARCH}" --with-libevent-dir="${BUILT_PRODUCTS_DIR}" --with-openssl-dir="${BUILT_PRODUCTS_DIR}" --enable-lzma CC="$(xcrun -f --sdk ${PLATFORM_NAME} clang) -arch ${ARCH}" CPP="$(xcrun -f --sdk ${PLATFORM_NAME} clang) -E -arch ${ARCH}" CPPFLAGS="${DEBUG_CFLAGS} ${BITCODE_CFLAGS} -I${SRCROOT}/Tor/openssl/include -I${BUILT_PRODUCTS_DIR}/openssl-${ARCH} -I${SRCROOT}/Tor/libevent/include -I${BUILT_PRODUCTS_DIR}/libevent-${ARCH} -I${BUILT_PRODUCTS_DIR}/liblzma-${ARCH} -I${PSEUDO_SYS_INCLUDE_DIR} -isysroot ${SDKROOT}" cross_compiling="yes" ac_cv_func__NSGetEnviron="no" ac_cv_func_clock_gettime="no" ac_cv_func_getentropy="no" LDFLAGS="-lz ${BITCODE_CFLAGS}"
+    ./configure --enable-restart-debugging --disable-tool-name-check --disable-unittests --enable-static-openssl --enable-static-libevent --disable-asciidoc --disable-system-torrc --disable-linker-hardening --disable-dependency-tracking --prefix="${CONFIGURATION_TEMP_DIR}/tor-${ARCH}" --with-libevent-dir="${BUILT_PRODUCTS_DIR}" --with-openssl-dir="${BUILT_PRODUCTS_DIR}" --enable-lzma CC="$(xcrun -f --sdk ${PLATFORM_NAME} clang) -arch ${ARCH}" CPP="$(xcrun -f --sdk ${PLATFORM_NAME} clang) -E -arch ${ARCH}" CPPFLAGS="${DEBUG_CFLAGS} ${BITCODE_CFLAGS} -I${SRCROOT}/Tor/tor/core -I${SRCROOT}/Tor/openssl/include -I${BUILT_PRODUCTS_DIR}/openssl-${ARCH} -I${SRCROOT}/Tor/libevent/include -I${BUILT_PRODUCTS_DIR}/libevent-${ARCH} -I${BUILT_PRODUCTS_DIR}/liblzma-${ARCH} -I${PSEUDO_SYS_INCLUDE_DIR} -isysroot ${SDKROOT}" cross_compiling="yes" ac_cv_func__NSGetEnviron="no" ac_cv_func_clock_gettime="no" ac_cv_func_getentropy="no" LDFLAGS="-lz ${BITCODE_CFLAGS}"
 
     mkdir -p "${BUILT_PRODUCTS_DIR}/tor-${ARCH}"
+    mkdir -p "${CONFIGURATION_TEMP_DIR}/tor-${ARCH}"
     cp orconfig.h "${CONFIGURATION_TEMP_DIR}/tor-${ARCH}/orconfig.h"
     cp orconfig.h "${BUILT_PRODUCTS_DIR}/tor-${ARCH}/orconfig.h"
 
@@ -73,28 +109,54 @@ do
 
     cp micro-revision.i "${BUILT_PRODUCTS_DIR}/tor-${ARCH}/micro-revision.i"
 
-    for LIBRARY in src/common/*.a src/or/*.a src/ext/ed25519/donna/*.a src/ext/ed25519/ref10/*.a src/trunnel/*.a src/ext/keccak-tiny/*.a;
+    for LIBRARY in src/lib/*.a src/core/*.a src/ext/ed25519/donna/*.a src/ext/ed25519/ref10/*.a src/trunnel/*.a src/ext/keccak-tiny/*.a;
     do
         cp $LIBRARY "${BUILT_PRODUCTS_DIR}/$(basename ${LIBRARY} .a).${ARCH}.a"
     done
+    make clean
     make distclean
 done
 
 cp -rf "${BUILT_PRODUCTS_DIR}/tor-${ARCHS[0]}" "${BUILT_PRODUCTS_DIR}/tor"
 
 # Combine the built products into a fat binary
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libtor."*.a -output "${BUILT_PRODUCTS_DIR}/libtor.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libor."*.a -output "${BUILT_PRODUCTS_DIR}/libor.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libor-event."*.a -output "${BUILT_PRODUCTS_DIR}/libor-event.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libor-crypto."*.a -output "${BUILT_PRODUCTS_DIR}/libor-crypto.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libor-ctime."*.a -output "${BUILT_PRODUCTS_DIR}/libor-ctime.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libor-trunnel."*.a -output "${BUILT_PRODUCTS_DIR}/libor-trunnel.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libcurve25519_donna."*.a -output "${BUILT_PRODUCTS_DIR}/libcurve25519_donna.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libed25519_donna."*.a -output "${BUILT_PRODUCTS_DIR}/libed25519_donna.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libed25519_ref10."*.a -output "${BUILT_PRODUCTS_DIR}/libed25519_ref10.a"
-xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/libkeccak-tiny."*.a -output "${BUILT_PRODUCTS_DIR}/libkeccak-tiny.a"
-rm "${BUILT_PRODUCTS_DIR}/libtor"*.*.a
-rm "${BUILT_PRODUCTS_DIR}/libor"*.*.a
-rm "${BUILT_PRODUCTS_DIR}/libcurve25519_donna."*.a
-rm "${BUILT_PRODUCTS_DIR}/libed25519"*.*.a
-rm "${BUILT_PRODUCTS_DIR}/libkeccak-tiny."*.a
+for LIB in \
+    libed25519_ref10 \
+    libed25519_donna \
+    libkeccak-tiny \
+    libtor-err \
+    libtor-ctime \
+    libtor-compress \
+    libtor-container \
+    libtor-crypt-ops \
+    libtor-encoding \
+    libtor-evloop \
+    libtor-fdio \
+    libtor-fs \
+    libcurve25519_donna \
+    libtor-intmath \
+    libtor-lock \
+    libtor-log \
+    libtor-math \
+    libtor-memarea \
+    libtor-meminfo \
+    libtor-malloc \
+    libtor-net \
+    libtor-osinfo \
+    libtor-process \
+    libtor-sandbox \
+    libtor-string \
+    libtor-smartlist-core \
+    libtor-term \
+    libtor-thread \
+    libtor-time \
+    libtor-tls \
+    libtor-trace \
+    libtor-wallclock \
+    libor-trunnel \
+    libtorrunner \
+    libtor-app;
+do
+    xcrun --sdk $PLATFORM_NAME lipo -create "${BUILT_PRODUCTS_DIR}/${LIB}."*.a -output "${BUILT_PRODUCTS_DIR}/${LIB}.a"
+    rm "${BUILT_PRODUCTS_DIR}/${LIB}."*.a
+done
